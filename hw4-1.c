fc26818d5db5395c8ac3a8e32c8b9d0b41fce45b
112310247 <146065161+112310247@users.noreply.github.com>
2023-10-28 17:22:36 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int panduan(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2;i <= sqrt(num);i++) {
+        if (num%i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("輸入要判斷幾個數字");
+    scanf("%d", &n);
+
+    int num;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        if (panduan(num)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
 
